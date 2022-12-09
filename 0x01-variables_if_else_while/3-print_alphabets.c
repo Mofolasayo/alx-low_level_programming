a547de9e73b3da42a282939d20c123ae0ad5e780
Mofolasayo <mofolasayoosikoya5@gmail.com>
2022-12-09 06:34:10 -0800

    all alphabeths

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *
+ *Return: 0 (success)
+ */
+
+int main(void)
+{
+char i, j;
+for (i = 'a'; i <= 'z'; i++)
+{
+putchar(i);
+}
+for (j = 'A'; j <= 'Z'; j++)
+{
+putchar(j);
+}
+putchar('\n');
+return (0);
+}
