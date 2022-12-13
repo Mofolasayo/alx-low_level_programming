81d68d744174009d5ef79c713950a85b1a4c0175
Mofolasayo <mofolasayoosikoya5@gmail.com>
2022-12-13 14:39:08 -0800

    file 11

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,13 +9,14 @@ void print_to_98(int n)
 {
 if (n < 98)
 {
-for (n = n; n <= 98; n ++)
+for (n = n; n <= 98; n++)
 {
 if (n == 98)
 printf("%d", n);
 else
 printf("%d, ", n);
 }
+printf("\n");
 }
 else if (n > 98)
 {
@@ -26,5 +27,6 @@ printf("%d", n);
 else
 printf("%d, ", n);
 }
+printf("\n");
 }
 }
