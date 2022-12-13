d99cf0ba48c7ee8f3bc4929d2d4e6aa42b2719fc
Mofolasayo <mofolasayoosikoya5@gmail.com>
2022-12-13 10:54:53 -0800

    file 0 and 1

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *main - entry point
+ *
+ *Return: 0 (success)
+ */
+
+int main(void)
+{
+char word[] = "_putchar";
+int i;
+
+for (i = 0; i <= 7; i++)
+{
+_putchar(word[i]);
+}
+_putchar('\n');
+return (0);
+}
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *print_alphabet - prints alphabeth in lowercase
+ *
+ *Return: nothing
+ */
+
+void print_alphabet(void)
+{
+char i;
+for (i = 97; i <= 122; i++)
+{
+_putchar(i);
+}
+}
