03978c72886a1475ecd906017e2dc355e6a0728a
Mofolasayo <mofolasayoosikoya5@gmail.com>
2022-12-13 11:46:23 -0800

    print_alphabetx10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - prints the alphabet in lowercase ten times
+ *
+ *Return: nothing
+ */
+
+void print_alphabet_x10(void)
+{
+int i;
+i = 0;
+while (i < 10)
+{
+char j;
+for (j = 97; j <= 122; j++)
+{
+_putchar(j);
+}
+i++
+}
+_putchar('\n');
+}
