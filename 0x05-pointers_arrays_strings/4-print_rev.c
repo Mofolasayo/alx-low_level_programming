1a4fae7a45b143e7d94cbe99461733162e3a60a2
Mofolasayo <mofolasayoosikoya5@gmail.com>
2022-12-19 12:15:39 -0800

    4-print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -27,9 +27,9 @@ return (i);
 void print_rev(char *s)
 {
 int i;
-for (i = (_strlen(str) - 1); i >= 0; i--)
+for (i = (_strlen(s) - 1); i >= 0; i--)
 {
-_putchar(str[i]);
+_putchar(s[i]);
 }
 _putchar('\n');
 }
