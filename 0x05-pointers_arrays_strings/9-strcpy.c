dae73f28c91baec15b10c1b62c209db799cd5310
Mofolasayo <mofolasayoosikoya5@gmail.com>
2023-01-01 06:30:42 -0800

    betty coding style

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -2,18 +2,21 @@
 #include <stdio.h>
 
 /**
- *_strcpy - copies the string pointed to src including '\0' to the buffer 
+ *_strcpy - copies the string pointed to src including '\0' to the buffer
  *pointed to by dest
- *Return: 0 (success)
+ *Return: dest
+ *@dest: a char variable
+ *@src: a char variable
  */
 
 char *_strcpy(char *dest, char *src)
 {
-  int i;
-  for (i = 0; src[i] != '\0'; i++)
-    {
-      dest[i] = src[i];
-    }
-  dest[i] = '\0';
-  return (dest);
+int i;
+
+for (i = 0; src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
+dest[i] = '\0';
+return (dest);
 }
