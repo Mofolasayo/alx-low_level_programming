503b6a315709a706e4ac036be4f2b7d3ec94665d
Mofolasayo <mofolasayoosikoya5@gmail.com>
2023-01-02 00:51:04 -0800

    strlen

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,4 +1,20 @@
 #include "main.h"
+/**
+ *_strlen - return the length of a string
+ *@s: a pointer to a string of characters
+ *Return: i (length of the string)
+ */
+
+int _strlen(char *s)
+{
+int i;
+i = 0;
+while (s[i] != '\0')
+{
+i++;
+}
+return (i);
+}
 
 /**
  *_strcat - concatenates two strings overwriting '\0'
