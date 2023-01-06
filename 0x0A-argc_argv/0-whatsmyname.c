faa67bc279044250fd1be625badbc8ff9c885c66
Mofolasayo <115883934+Mofolasayo@users.noreply.github.com>
2023-01-06 21:57:13 +0100

    Update 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,10 +2,10 @@
 #include <stdio.h>
 
 /**
- * main - prints the name of the pprogram
- * @argc: Number of comand line arguments
- * @argv: array
- * Return: 0
+ *main - prints the name of the pprogram
+ *@argc: Number of comand line arguments
+ *@argv: array
+ *Return: 0
  */
 
 int main(int argc, char *argv[])
