62fa257d43d20271dc2945d8eddf85c7466d2b89
Mofolasayo <mofolasayoosikoya5@gmail.com>
2023-01-14 02:34:26 -0800

    fizzbuzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *fizzbuzz - prints numbers from 1 - 100 while checking for the multiples of 3 and 5
+ *Return: nothing
+ */
+
+void fizzbuzz(void)
+{
+int i;
+for (i = 1; i <= 100; i++)
+{
+if (i % 3 == 0 && i % 5 == 0)
+{
+printf("FizzBuzz ");
+}
+else if (i % 3 == 0)
+{
+printf("Fizz ");
+}
+else if (i % 5 == 0)
+{
+printf("Buzz ");
+}
+else
+printf("%d ", i);
+}
+}
