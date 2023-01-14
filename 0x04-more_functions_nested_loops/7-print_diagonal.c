fe74e20a28653e39c6553ba1d56256d7d561b0ea
Mofolasayo <mofolasayoosikoya5@gmail.com>
2023-01-14 01:54:26 -0800

    betty coding style

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,13 +8,15 @@
 
 void print_diagonal(int n)
 {
-if (n <= 0){
+if (n <= 0)
+{
 _putchar('\n');
 }
 else
 {
 int i, j;
-for (i = 0; i < n; i++){
+for (i = 0; i < n; i++)
+{
 for (j = 1; j <= i; j++)
 {
 _putchar(' ');
@@ -22,5 +24,5 @@ _putchar(' ');
 _putchar('\\');
 _putchar('\n');
 }
-}  
+}
 }
